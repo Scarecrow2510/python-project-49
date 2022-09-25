2cb24204c22313a3388b562a9c57a9fb3dbb6d3b
Denis Shchukin <denis@DenisPC.localdomain>
2022-09-25 16:35:14 +0300

    Коммит третьего упражнения

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+from brain_games.cli import *
 
 def main():
     print("Welcome to the Brain Games!")
+    welcome_user()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
