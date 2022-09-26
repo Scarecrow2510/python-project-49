bf271c4af24994c16fbafdd5acfbd3de615f7995
Denis Shchukin <denis@DenisPC.localdomain>
2022-09-26 19:06:25 +0300

    Коммит пятого шага

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -5,7 +5,6 @@ import prompt
 def main():
     print("Welcome to the Brain Games!")
 
-
     def welcome_user():
         name = prompt.string('May I have your name? ')
         print('Hello, ' + name + '!')
@@ -14,7 +13,6 @@ def main():
         Cor_Sum = 0
         i = 1
 
-
         while i <= 3:
             ran = randint(0, 99)
             print('Question: ' + str(ran))
@@ -22,9 +20,9 @@ def main():
 
             if ran % 2 != 0 and answer == 'yes':
                 print('"yes" if wrong answer ;(. Correct answer was "no".\n'
-                        'Let\'s try again, ' + name + '!')
+                      'Let\'s try again, ' + name + '!')
                 break
-    
+
             elif ran % 2 != 0 and answer == 'no':
                 print('Correct!')
                 i = i + 1
@@ -32,7 +30,7 @@ def main():
 
             elif ran % 2 != 0:
                 print(answer + 'is wrong answer ;(. Correct answer was "no".\n'
-                        'Let\'s try again, ' + name + '!')
+                               'Let\'s try again, ' + name + '!')
                 break
 
             else:
@@ -40,25 +38,22 @@ def main():
                     print('Correct!')
                     i = i + 1
                     Cor_Sum = Cor_Sum + 1
-       
+
                 elif ran % 2 == 0 and answer == 'no':
                     print('"no" is wrong answer ;(. Correct answer was "yes".\n'
-                            'Let\'s try again, ' + name + '!')
+                          'Let\'s try again, ' + name + '!')
                     break
 
                 else:
                     print(answer + ' is wrong answer ;(. Correct answer was "yes".\n'
-                            'Let\'s try again, ' + name + '!')
+                                   'Let\'s try again, ' + name + '!')
                     break
 
-
         if Cor_Sum == 3:
-            print('Congratulations, ' + name +  '!')
-
+            print('Congratulations, ' + name + '!')
 
     welcome_user()
 
 
 if __name__ == '__main__':
     main()
-    
