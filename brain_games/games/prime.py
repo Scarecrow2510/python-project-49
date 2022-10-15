64e29fae5b93a2e1977f56bdda574785c3537868
Denis Shchukin <denis@DenisPC.localdomain>
2022-10-15 16:50:57 +0300

    Финальный коммит

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -9,15 +9,15 @@ description = 'Answer "yes" if given number is prime. ' \
 def is_prime(number):
     if number == 1:
         return 'no'
-    for i in range(2, (number // 2 + 1)):
-        if number % 1 == 0:
+    for i in range(2, (number // 2 + 1)):i
+        if number % i == 0:
             return 'no'
     return 'yes'
 
 
 def make_all_game():
     min_number = 1
-    max_number = 21
+    max_number = 100
     number = randint(min_number, max_number)
     question = str(number)
     return question, is_prime(number)
