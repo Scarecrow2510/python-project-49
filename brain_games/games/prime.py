21874155a14fa831975d188c0b734df334b0d55b
Denis Shchukin <denis@DenisPC.localdomain>
2022-10-18 16:09:32 +0300

    Финальный коммит доработок

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,19 +5,23 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if given number is prime. ' \
               'Otherwise answer "no".'
 MIN_NUMBER = 1
-MAX_NUMBER - 100
+MAX_NUMBER = 100
 
 
 def is_prime(number):
     if number == 1:
-        return 'no'
+        return False
     for i in range(2, (number // 2 + 1)):
         if number % i == 0:
-            return 'no'
-    return 'yes'
+            return False
+    return True
 
 
 def make_game():
     number = randint(MIN_NUMBER, MAX_NUMBER)
     question = str(number)
-    return question, is_prime(number)
+
+    if is_prime(number) is False:
+        return question, 'no'
+    else:
+        return question, 'yes'
